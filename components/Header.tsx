c9afbcb5e33f04deacf694138c4c77f3709b699a
filmaworkspace <filmaworkspace@proton.me>
2025-12-31 01:32:32 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -200,7 +200,7 @@ export default function Header() {
         {/* Logo */}
         <Link href="/dashboard" className="select-none flex items-center gap-2">
           <Image
-            src="/logo.svg"
+            src="/logodark.svg"
             alt="Logo"
             width={100}
             height={24}
